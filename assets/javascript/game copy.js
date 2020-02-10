31d32a830625ceca426e07a58b725b757b32f35c
jvanhook13 <jaredvanhook1313@gmail.com>
2020-02-10 03:56:19 -0800

    same problem diff code. tru=ying to set a function up but may just go back to loop i dunno cant break my loop cant get loop to work with function though....

--- assets/javascript/game copy.js
+++ assets/javascript/game copy.js
@@ -2,57 +2,108 @@ var randomWords = ["luffy", "naruto", "tanjiro", "ichigo", "kirito", "kaneki"];
 
 var word = randomWords[Math.floor(Math.random() * randomWords.length)];
 
-console.log ( "word" , word);
+var lettersGuessed = [];
+var remainingGuesses = 6;
+var wins = 0;
+var displayWord = []
+
+
+console.log("word", word);
 
 var answerArray = word.split("");
 
 
 
-console.log( "Answerarray" , answerArray) ;
+console.log("Answerarray", answerArray);
 
 var blanks = answerArray.length;
 
-console.log("blanks" , blanks)
+console.log("blanks", blanks)
+// sets game loop
 
-while (blanks > 0) {
-
-    var playerInput = prompt("Please guess a letter").toLowerCase() ;
-    if (playerInput === null) {
-        break;
-
-    } else if (playerInput.length !== 1) {
-        alert("Please enter one letter") ;
-
-
-    }  
-
-    else {
-        
-       for ( var i = 0;  i < answerArray.length; i++) {
-           if (word[i] == playerInput) {
-                console.log( "word[i]" , word[i])
-                console.log( "player input" , playerInput)
-
-
-
-
-           }
-
-
-
-
-
-    }} ;
-
-    // console.log( "player input" , playerInput)
-
-
-
-    // while the blanks are greater than zero 
-    // take playerInput and check word to see if playerInput is equal to 
-
-
-
-
+for (var i = 0; i < blanks; i++) {
+    displayWord.push("_");
+    console.log("displayWord", displayWord)
 }
 
+gameLoop:
+
+    do {
+
+
+        // gives prompt to user and what to do with the input
+        var playerInput = prompt("Please guess a letter").toLowerCase();
+        if (playerInput === null) {
+            break;
+            // in case of extra letter
+        } else if (playerInput.length !== 1) {
+            alert("Please enter one letter");
+
+
+        } else {
+
+            //sets function to check for right answer
+
+            function plzWork() {
+                var inWord = false;
+                for (var i = 0; i < answerArray.length; i++) {
+                    if (word[i] == playerInput) {
+                        inWord = true;
+
+
+
+                    }
+
+
+                    if (inWord) {
+                        for (var i = 0; i < blanks; i++) {
+                            if (word[i] == playerInput) {
+                                answerArray[i] == playerInput;
+
+
+                            } else {
+
+                                lettersGuessed.push(playerInput);
+                                remainingGuesses--;
+                            };
+
+                            console.log(lettersGuessed, "letters guessed");
+                            console.log("remainingguesses", remainingGuesses);
+
+
+
+                        }
+                    }
+                    //breaks loop so it doesnt iterate through whole array
+
+
+
+
+
+
+
+                }
+
+
+            }
+
+
+
+
+            while (blanks > 0)
+
+
+
+
+
+            // console.log( "player input" , playerInput)
+
+
+
+            // while the blanks are greater than zero 
+            // take playerInput and check word to see if playerInput is equal to a value in answerArray
+            // if it is decrease blanks by one and then display guessed letter in html
+            // if it not equal then remove a guess left and list the letter to the side in html
+            //   
+
+    
